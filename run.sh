cd960188bc9ab5e8a4e523436a4c8334ee42cf7d
aBerglehner <alexanderberglehner2@gmail.com>
2026-01-27 01:21:36 +0100

    feat: adjusted run script

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ tmux split-window -v
 tmux split-window -v
 tmux select-layout even-vertical
 
-tmux send-keys -t 1 'sleep 2; clear; go run client/client.go localhost 9090 ' C-m
-tmux send-keys -t 2 'sleep 2; clear; go run client/client.go localhost 9090 ' C-m
+tmux send-keys -t 1 'sleep 1; clear; go run client/client.go localhost 9090 ' C-m
+tmux send-keys -t 2 'sleep 1; clear; go run client/client.go localhost 9090 ' C-m
 go run server/server.go 9090
