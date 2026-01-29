94ada6456ce79eda6879193ef29ef22fbde767a7
aBerglehner <alexanderberglehner2@gmail.com>
2026-01-30 00:10:43 +0100

    feat: update run script

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 tmux split-window -v
-tmux split-window -v
-tmux select-layout even-vertical
+tmux split-window -h
+tmux select-layout main-horizontal
 
 #will be player 2
 tmux send-keys -t 1 'sleep 1.5; clear; go run client/client.go localhost 9090 ' C-m
